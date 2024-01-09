3aac3780a4c08cc83c62dce60e344deb150342e6
Dobbymin <gmin0701@knu.ac.kr>
2024-01-09 16:59:43 +0900

    ✨ feat: 한번에 import 하기

--- week-01/JS-project-01/assets/scripts/app.js
+++ week-01/JS-project-01/assets/scripts/app.js
@@ -1,3 +1,8 @@
-import { apiKey } from './util.js';
+// import { apiKey } from './util.js';
 
-console.log(apiKey);
+// import { apiKey, name } from './util.js';
+
+import * as util from './util.js';
+
+console.log(util.apiKey);
+console.log(util.name);
