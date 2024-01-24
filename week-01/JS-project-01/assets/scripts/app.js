67ed355b4e8434723ea388591bf12b782a2e2080
Dobbymin <gmin0701@knu.ac.kr>
2024-01-24 14:39:46 +0900

    ✨ Feat: JS 문법 복습 끝

--- week-01/JS-project-01/assets/scripts/app.js
+++ week-01/JS-project-01/assets/scripts/app.js
@@ -1,21 +1,9 @@
-const userNameData = ['min', 'dobby'];
+function init() {
+    function greet() {
+        console.log('Hi!');
+    }
 
-// const firstName = userNameData[0];
-// const lastName = userNameData[1];
+    greet();
+}
 
-const [firstName, lastName] = ['min', 'dobby'];
-
-console.log(firstName, lastName);
-
-// const user = {
-//     name: 'dobby',
-//     age: 26,
-// };
-
-// const name = user.name;
-// const age = user.age;
-
-const { name: userName, age } = {
-    name: 'dobby',
-    age: 26,
-};
+greet();
